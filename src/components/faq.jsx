f95b6117f90a80de5bb2063335478ef8ea897d90
Majekodunmi Pelumi <164484907+Pelumipeace@users.noreply.github.com>
2024-10-08 15:20:02 +0100

    feat/pelumi/finale

--- src/components/faq.jsx
+++ src/components/faq.jsx
@@ -18,7 +18,7 @@ function Faq() {
             </p>
           </div>
           <div className="mt-[45px] flex justify-center">
-            <div className="w-[600px] sixm:w-full flex flex-col gap-[12px]">
+            <div className="w-[800px] sixm:w-full flex flex-col gap-[12px]">
               {faqData?.map((item, i) => (
                 <div
                   key={i}
